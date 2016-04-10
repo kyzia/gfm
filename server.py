df34b09eb5fc926b7f91a736e743ed9765b135c6
Oleg Kuznetsov <smalloleg@gmail.com>
2016-04-11 00:25:45 +0300

    Add date, subject, try to decode message and get text from html

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ from flask import Flask, request
 from json import loads, dumps
 from yaml import load
 
-from utils import get_first_text_block, download_attachments_in_email
+from utils import get_first_text_block, download_attachments_in_email,get_alltext_block,convert2text
 import email
 import imaplib
 
@@ -18,26 +18,23 @@ with open(CONF) as f:
         conf = load("".join(f.readlines()))
 
 ACCOUNTS_DICT=conf['cfg']['accounts']
-print ACCOUNTS_DICT
 
 
 
 @app.route("/api/getthefuckingmail", methods=('POST',))
 def getthefuckingmaiil():
-    print request
     if request.form:
         data, = request.form.keys()
     elif request.data:
         data = request.data
-
     data = loads(data)
-#    data = loads(data)
-#    return dumps(data)
+    print("Request is: {0}".format(data))
     return getmail(data)
 
 
 def getmail(data):
     account=data['account']
+    rootAttachdir=ACCOUNTS_DICT[account]['rootAttachdir']
     server=ACCOUNTS_DICT[account]['server']
     login=ACCOUNTS_DICT[account]['login']
     password=ACCOUNTS_DICT[account]['password']
@@ -48,15 +45,13 @@ def getmail(data):
     # Out: list of "folders" aka labels in gmail.
     mail.select("inbox", readonly=True) # connect to inbox.
 
-
     result, data = mail.uid('search', None, "(UNSEEN)") # search and return uids instead
-    print data
     uid_list = data[0].split()
+    print uid_list
     uid_list.reverse()
 
     if not uid_list:
         return '{"Error":"empty_uidlist"}'
-    print uid_list
 
     response_dict={}
     for uid in uid_list:
@@ -64,14 +59,17 @@ def getmail(data):
         result, datamail = mail.uid('fetch', uid, '(RFC822)')
         raw_email = datamail[0][1]
         email_message = email.message_from_string(raw_email)
-        response_dict[uid]['text']=get_first_text_block(email_message)
+        response_dict[uid]['text']=get_alltext_block(email_message)
 
-        curent_dir='{0}/{1}/{2}'.format(os.getcwd(),account,uid)
-        os.makedirs(curent_dir)
+        curent_dir='{0}/{1}/{2}'.format(rootAttachdir,account,uid)
+        if not os.path.exists(curent_dir):
+            os.makedirs(curent_dir)
 
         response_dict[uid]['attachments']=download_attachments_in_email(email_message, curent_dir)
-#        print
-#    response_dict['attachments']=attach_dict
+        response_dict[uid]['date']=email.utils.parsedate(email_message.get('date'))        
+        response_dict[uid]['Subject']=email_message.get('Subject')
+        response_dict[uid]['From']=email_message.get('From')
+        response_dict[uid]['To']=email_message.get('To')
     return dumps(response_dict)
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -5,13 +5,37 @@ def get_first_text_block(email_message_instance):
             if part.get_content_maintype() == 'text':
                 return part.get_payload()
     elif maintype == 'text':
+        maintext=email_message_instance.get_payload()
+                
         return email_message_instance.get_payload()
+
+def get_alltext_block(email_message_instance):
+#    for part in email_message_instance.walk() 
+    maintype = email_message_instance.get_content_maintype()
+    returndict={}
+    if maintype == 'multipart':
+        num=0
+        for part in email_message_instance.walk():
+            charset = part.get_content_charset()
+            if part.get_content_maintype() == 'text':
+                string = part.get_payload(decode=1)
+                if charset:
+                    returndict[num]= convert2text(string.decode(charset))
+                else:
+                    returndict[num]= convert2text(string)
+                num+=1
+        return returndict
+    elif maintype == 'text':
+        charset = email_message_instance.get_content_charset()
+        string=email_message_instance.get_payload(decode=1)
+        returndict['0']=convert2text(string.decode(charset))
+        return returndict
 
 def download_attachments_in_email(email_message, outputdir):
     num=0
     returndict={}
     if email_message.get_content_maintype() != 'multipart':        
-        return 'MAintype is not multipart'
+        return 'Maintype is not multipart'
     for part in email_message.walk():
         if part.get_content_maintype() != 'multipart' and part.get('Content-Disposition') is not None:
             open(outputdir + '/' + part.get_filename(), 'wb').write(part.get_payload(decode=True))
@@ -19,3 +43,7 @@ def download_attachments_in_email(email_message, outputdir):
             num+=1
     return returndict
 
+def convert2text(message):
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(message)
+    return soup.get_text('\n')
